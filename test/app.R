8676e2e7ac09b831d8bffa64ad88c777e42d3ea0
dax44 <dax44@poczta.onet.pl>
2024-02-20 22:37:09 +0100

    Shiny done

--- test/app.R
+++ test/app.R
@@ -1,73 +1,37 @@
 library(shiny)
-library(vroom)
-library(janitor)
+library(dplyr)
+library(ggplot2)
 
 # UI
 ui <- fluidPage(
-  titlePanel("Przetwarzanie i pobieranie danych"),
-  
-  # Upload i parsowanie pliku
+  titlePanel("Dynamiczne filtrowanie danych z użyciem any_of/all_of"),
   sidebarLayout(
     sidebarPanel(
-      fileInput("file", "Wybierz plik danych", accept = ".csv"),
-      textInput("delim", "Separator (domyślnie ,)", value = ","),
-      numericInput("skip", "Pomiń wiersze", 0),
-      numericInput("rows", "Podgląd wierszy", 5),
-      checkboxInput("snake", "Nazwy kolumn do snake_case"),
-      checkboxInput("constant", "Usuń kolumny stałe"),
-      checkboxInput("empty", "Usuń puste kolumny"),
-      downloadButton("download", "Pobierz przetworzone dane")
+      selectInput("vars", "Wybierz zmienne:", choices = names(mtcars), multiple = TRUE),
+      numericInput("min_value", "Minimalna wartość:", 0),
+      actionButton("filter_btn", "Filtruj")
     ),
     mainPanel(
-      tabsetPanel(
-        tabPanel("Podgląd surowych danych", tableOutput("preview_raw")),
-        tabPanel("Podgląd oczyszczonych danych", tableOutput("preview_clean"))
-      )
+      DT::dataTableOutput("filtered_table")
     )
   )
 )
 
 # Server
 server <- function(input, output) {
-  # Odczytanie i podgląd surowych danych
-  data_raw <- reactive({
-    req(input$file)
-    read.csv(input$file$datapath, sep = input$delim, skip = input$skip)
+  observeEvent(input$filter_btn, {
+    filtered_data <- reactive({
+      req(input$vars) # upewniamy się, że co najmniej jedna zmienna została wybrana
+      # Używamy any_of, aby uniknąć błędów, jeśli wybrana zmienna nie istnieje
+      mtcars %>%
+        filter(across(any_of(input$vars), ~ . >= input$min_value))
+    })
+    
+    output$filtered_table <- DT::renderDataTable({
+      filtered_data()
+    })
   })
-  
-  output$preview_raw <- renderTable({
-    head(data_raw(), n = input$rows)
-  })
-  
-  # Oczyszczanie danych
-  data_clean <- reactive({
-    df <- data_raw()
-    if (input$snake) {
-      df <- df %>% janitor::clean_names(case = "snake")
-    }
-    if (input$constant) {
-      df <- df %>% janitor::remove_constant()
-    }
-    if (input$empty) {
-      df <- df %>% janitor::remove_empty("cols")
-    }
-    df
-  })
-  
-  output$preview_clean <- renderTable({
-    head(data_clean(), n = input$rows)
-  })
-  
-  # Pobieranie oczyszczonych danych
-  output$download <- downloadHandler(
-    filename = function() {
-      paste(Sys.Date(), "-clean-data.csv", sep = "")
-    },
-    content = function(file) {
-      write.csv(data_clean(), file, row.names = FALSE)
-    }
-  )
 }
 
 # Uruchomienie aplikacji
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
